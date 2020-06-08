bc5568dbe468978e353a2ab45b074de397394efd
sanjeeb naik <sanjeebk@in.ibm.com>
2020-06-09 00:58:00 +0530

    Added another geturl to test

--- nodejs-app/app.js
+++ nodejs-app/app.js
@@ -4,4 +4,6 @@ const app=express()
 
 app.get("/",(req,res) => res.send("Hello Sanjeeb!"))
 
+app.get("/welcome",(req,res) => res.send("Welcome Sanjeeb!"))
+
 app.listen(8080,(req,res) => console.log("server started"))
